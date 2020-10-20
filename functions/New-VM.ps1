ecaeaa19ec7cf4743c626c1c66e61c279c3932d4
Jan-Hendrik Peters <jhp@MinasMorgul.middleearth>
2020-10-20 15:11:28 +0200

    More function stubs

--- functions/New-VM.ps1
+++ functions/New-VM.ps1
@@ -13,10 +13,13 @@ function New-Vm
         [Parameter(Mandatory)]
         [uint64]
         $Memory,
-
+ 
         [Parameter(Mandatory)]
         [uint16]
-        $MaxCpu,
+        $CpuCount,
+
+        [uint16]
+        $MaxCpuCount,
 
         [uint16]
         $Sockets,
@@ -34,7 +37,7 @@ function New-Vm
         $CpuSet,
 
         [LibVirtDiskConfiguration[]]
-        $Disk,
+        $StorageConfiguration,
 
         [string]
         $OsType,
@@ -63,12 +66,15 @@ function New-Vm
         [LibVirtNetwork[]]
         $NetworkConfiguration,
 
+        [Parameter(ParameterSetName = 'fv')]
         [switch]
         $FullVirtualization,
 
+        [Parameter(ParameterSetName = 'pv')]
         [switch]
         $ParaVirtualization,
 
+        [Parameter(ParameterSetName = 'container')]
         [switch]
         $Container,
 
@@ -85,4 +91,51 @@ function New-Vm
     # AL Windows: Floppy mounten unattend druff
     # AL WIM file? Apply??
     # AL Linux: dd /if /of? oder ISO + ks.cfg/autoyast
+
+    $commandLine = @(
+        "--name=$ComputerName"
+        if (-not [string]::IsNullOrWhiteSpace($Description)) { "--description=$Description" }
+        "--ram=$($Memory/1MB)"
+        if ($CpuSet.Count -gt 0) {"--cpuset=$($CpuSet -join ',')"}
+        if (-not [string]::IsNullOrWhiteSpace($CdRom)) {"--cdrom==$CdRom"}
+        if (-not [string]::IsNullOrWhiteSpace($InstallationSource)) {"--location=$InstallationSource"}
+        if ($PxeBoot.IsPresent) {"--pxe"}
+        if ($Import.IsPresent) {"--import"}
+        if ($NoApic.IsPresent) {"--noapic"}
+        if ($NoAcpi.IsPresent) {"--noacpi"}
+        if ($FullVirtualization.IsPresent) {"--hvm"}
+        if ($ParaVirtualization.IsPresent) {"--paravirt"}
+        if ($Container.IsPresent) {"--container"}
+        if (-not [string]::IsNullOrWhiteSpace($OsType)) {"--os-type=$OsType"}
+        if (-not [string]::IsNullOrWhiteSpace($OsVariant)) {"--os-variant=$OsVariant"}
+    )
+
+    $cpuString = "--vcpus=$CpuCount"
+    if ($MaxCpuCount -gt 0) {$cpuString += ",maxvcpus=$MaxCpuCount"}
+    if ($Sockets -gt 0) {$cpuString += ",sockets=$Sockets"}
+    if ($Cores -gt 0) {$cpuString += ",cores=$Cores"}
+    if ($Threads -gt 0) {$cpuString += ",threads=$Threads"}
+    $commandLine += $cpuString
+
+    foreach ($disk in $StorageConfiguration)
+    {
+        $commandLine += $disk.ToString()
+    }
+
+    if ($StorageConfiguration.Count -eq 0)
+    {
+        $commandLine += '--nodisks'
+    }
+
+    foreach ($nic in $NetworkConfiguration)
+    {
+
+    }
+
+    if ($NetworkConfiguration.Count -eq 0)
+    {
+        $commandLine += '--nonetworks'
+    }
+
+    Start-Process -FilePath 'virt-install' -ArgumentList $commandLine -Wait
 }
