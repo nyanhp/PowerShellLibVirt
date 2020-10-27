191c804229bc46535ee6973da1d42301bc7eb786
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2020-10-27 18:49:20 +0100

    String surplus

--- PoshLibVirt/internal/functions/Clear-PSBoundParameters.ps1
+++ PoshLibVirt/internal/functions/Clear-PSBoundParameters.ps1
@@ -1,4 +1,16 @@
-﻿function Clear-PSBoundParameters
+﻿<#
+.SYNOPSIS
+    Clear CommonParameters from Dictionary
+.DESCRIPTION
+    Clear CommonParameters from Dictionary
+.PARAMETER ParameterDictionary
+    The dictionary to clean up
+.EXAMPLE
+    Clear-PSBoundParameters -ParameterDictionary $PSBoundParameters
+
+    Clears the PSBoundParameters dictionary
+#>
+function Clear-PSBoundParameters
 {
     [CmdletBinding()]
     param
